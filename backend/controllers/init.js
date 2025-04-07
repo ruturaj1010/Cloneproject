dedac48c7b45f4ed577c3c428f311c90599c671f
Ruturaj <ruturajnikam1010@gmail.com>
2025-04-08 01:37:32 +0530

    created init command functionality

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,21 @@
-const initRepo = () => {
-    console.log("Init command called");
+const fs = require("fs").promises;
+const path = require("path");
+
+const initRepo = async () => {
+    const repoPath = path.resolve(process.cwd(), ".MyGit");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitsPath, { recursive : true });
+        await fs.writeFile(
+            path.join(repoPath ,  "config.json"),
+            JSON.stringify({bucket : "S3 bucket"})
+        )
+        console.log("Repository initialized!")
+    } catch (error) {
+        console.error("Error whie initializing the repository");
+    }
 }
 
-module.exports = {initRepo};
+module.exports = { initRepo };
