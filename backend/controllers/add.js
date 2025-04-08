648528dbf79288bd80bcd36a8d5bfd473f8cdf66
Ruturaj <ruturajnikam1010@gmail.com>
2025-04-08 15:36:12 +0530

    created add command functionality

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,18 @@
-const addRepo = async () => {
-    console.log("add command called");
+const fs = require("fs").promises;
+const path = require("path");
+
+const addRepo = async (filepath) => {
+    const repoPath = path.resolve(process.cwd(), ".MyGit");
+    const stagePath = path.join(repoPath , "staging");
+
+    try {
+        await fs.mkdir(stagePath , {recursive:true});
+        const fileName = path.basename(filepath);
+        await fs.copyFile(filepath , path.join(stagePath, fileName));
+        console.log(`File ${fileName} added to the staging area`);
+    } catch (error) {
+        console.error("error in staging the files");
+    }
 }
 
 module.exports = {addRepo};
